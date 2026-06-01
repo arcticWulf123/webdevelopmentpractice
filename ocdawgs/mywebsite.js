f9315a492de62b95cf6adc2f286f66f8639cba18
arcticWulf123 <jaderolandeduard.pinon@lorma.edu>
2026-06-01 11:30:28 +0800

    removed else to prevent repeated no statements

--- ocdawgs/mywebsite.js
+++ ocdawgs/mywebsite.js
@@ -15,17 +15,15 @@ function square(number) {
 function twoSum(target) { //target is 17
     let nums = [7, 9, 12, 8]
     for (let i = 0; i < nums.length; i++) {
-        for (let j = (i+1); j < nums.length; j++) {
+        for (let j = i + 1; j < nums.length; j++) {
             if (nums[i] + nums[j] === target) {
                 console.log("solved")
                 break
-            } else {
-                console.log("no");
             }
         }
     }
 }
 
-console.log(factorial(5)); 
+console.log(factorial(5));
 console.log(square(5));
 console.log(twoSum(17));
