2513b8ae7d8f70fbbd3bcfc8ab3d69cf41d09ba2
arcticWulf123 <jaderolandeduard.pinon@lorma.edu>
2026-07-28 23:08:09 +0800

    react practice code

---
+++ react-test/src/App.jsx
@@ -0,0 +1,55 @@
+import "./App.css";
+
+const name = "Jade";
+function App() {
+  return (
+    // <div>
+    //   <Goodbye name="Jade" />
+    //   <Goodbye name="Roland" />
+    //   <NameAndAge name="Damian" age="17" />
+    // </div>
+    <div>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+function Goodbye(props) {
+  return <h1>Goodbye {props.name}</h1>;
+}
+
+export default App;
+
+function NameAndAge({ name, age }) {
+  return (
+    <h1>
+      Hello my name is {name}, and I am {age} years old
+    </h1>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <nav className="nav">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <div>
+      <input type="text" className="input" />
+    </div>
+  );
+}
+/* 
+1. Components are reusable in React
+2. Props act as the "argument" that a function can use
+3. Destructuring is a common practice in React, see function in line 20
+*/
