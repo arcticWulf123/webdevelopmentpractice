daec80ac5612bb8b8278ef41453b1ae2266f2259
arcticWulf123 <jaderolandeduard.pinon@lorma.edu>
2026-07-30 15:12:19 +0800

    made button component

---
+++ react-starbucks/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Button from "./components/Button"
+
+function App() {
+
+  return (
+    <div>
+      <Button text="Hello" background="blue" color="white"/>
+    </div>
+  )
+}
+
+export default App
---
+++ react-starbucks/src/components/Button.jsx
@@ -0,0 +1,22 @@
+function Button(props) {
+  return (
+    <a
+      href="#"
+      style={{
+        color: props.color,
+        borderRadius: 9999,
+        background: props.background,
+        textDecoration: "none",
+        fontSize: "small",
+        fontWeight: 500,
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        borderColor: props.borderColor,
+        padding: '8px 12px 8px 12px' /*  Top Right Bottom Left*/
+      }}
+    >
+      {props.text}
+    </a>
+  );
+}
+export default Button;
