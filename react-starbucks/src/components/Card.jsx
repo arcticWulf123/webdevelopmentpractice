b03693e1fc4015ed94bada4c275fc062d1047ad7
arcticWulf123 <jaderolandeduard.pinon@lorma.edu>
2026-08-02 23:22:53 +0800

    card started

---
+++ react-starbucks/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+function Card () {
+    return (
+        <div>
+            <img src="" alt="" />
+            <h1></h1>
+            <p></p>
+            <Button/>
+        </div>
+    )   
+}
+export default Card();
