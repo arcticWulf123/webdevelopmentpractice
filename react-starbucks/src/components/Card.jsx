8b611cdac141d78803744a514e8f93338ec23875
arcticWulf123 <jaderolandeduard.pinon@lorma.edu>
2026-08-05 16:45:11 +0800

    added flexbox to footer

--- react-starbucks/src/components/Card.jsx
+++ react-starbucks/src/components/Card.jsx
@@ -1,12 +1,18 @@
 import Button from "./Button";
-function Card (props) {
-    return (
-        <div>
-            <img src="" alt="" />
-            <h1>{props.header}</h1>
-            <p>{props.description}</p>
-            <Button/>
-        </div>
-    )   
+function Card(props) {
+  return (
+    <article
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "15px",
+      }}
+    >
+      <img src="" alt="" />
+      <h1>{props.header}</h1>
+      <p>{props.description}</p>
+      <Button />
+    </article>
+  );
 }
-export default Card();
+export default Card;
--- react-starbucks/src/components/Footer.jsx
+++ react-starbucks/src/components/Footer.jsx
@@ -1,67 +1,84 @@
 function Footer() {
   return (
     <>
-      <div>
-        <h1>About Us</h1>
-        <a href="">Our Company</a>
-        <a href="">Stories and News</a>
-        <a href="">Customer Service</a>
-        <a href="">Careers</a>
-      </div>
-      <div>
-        <h1>Order Online</h1>
-        <a href="">Order on the App</a>
-        <a href="">Delivery</a>
-        <a href="">Send eGifts</a>
-        <a href="">Grab Partnership</a>
-      </div>
-      <div>
-        <h1>Rewards</h1>
-        <a href="">My Account</a>
-        <a href="">View Transactions</a>
-        <a href="">Reload</a>
-        <a href="">FAQs</a>
-      </div>
-      <div>
-        <h1>Social Impact</h1>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-      </div>
-      <div>
-        <h1>Promotions</h1>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-        <a href=""></a>
-      </div>
-      <div>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
+      <div
+        className="container"
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          padding: "30px",
+          marginTop: "20px",
+          border: "1px solid grey"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        >
+          <h1>About Us</h1>
+          <a href="">Our Company</a>
+          <a href="">Stories and News</a>
+          <a href="">Customer Service</a>
+          <a href="">Careers</a>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        >
+          <h1>Order Online</h1>
+          <a href="">Order on the App</a>
+          <a href="">Delivery</a>
+          <a href="">Send eGifts</a>
+          <a href="">Grab Partnership</a>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+          }}
+        >
+          <h1>Rewards</h1>
+          <a href="">My Account</a>
+          <a href="">View Transactions</a>
+          <a href="">Reload</a>
+          <a href="">FAQs</a>
+        </div>
         <div>
-          <div>
-            <a href="">Web Accessiblity</a>
-            <a href="">Privacy Policy</a>
-            <a href="">Terms of Use</a>
-            <a href="">Site Map</a>
-            <a href="">Cookie Preferences</a>
-          </div>
-          <div>
-            <p>2022 Starbucks Coffee Company. All rights reserved.</p>
-          </div>
+          <h1>Social Impact</h1>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+        <div>
+          <h1>Promotions</h1>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </div>
+        <div> 
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
         </div>
       </div>
     </>
